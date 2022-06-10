a3fac89e40e024860cd0600f882df044cd3ad22d
sirdan <stanford303@gmail.com>
2022-06-10 13:30:51 -0700

    added my fighting monsters and buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [kingKongSize, setKingKongSize] = useState(10);
+  const [GodZillaSize, setGodZillaSize] = useState(10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='fight'>
+        <div className='monster'>
+          <img src="kong.png" width={21 * kingKongSize} />
+          <div className='buttons'>
+            <button onClick={() => setKingKongSize(kingKongSize + 1)}>Kong grows stronger</button>
+            <button onClick={() => setKingKongSize(kingKongSize - 1)}>kong weakens</button>
+          </div>
+        </div>
+        <div className='monsters'>
+          <img src="godzilla.png" width={13 * GodZillaSize} />
+          <div className='buttons'>
+            <button onClick={() => setGodZillaSize(GodZillaSize + 1)}>godzilla grows stronger</button>
+            <button onClick={() => setGodZillaSize(GodZillaSize - 1)}>godzilla is weak</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
